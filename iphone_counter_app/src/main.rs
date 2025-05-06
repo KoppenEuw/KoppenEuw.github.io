2487ba6573b47e987212de7c62321d74167d3bfe
Benjamin Lilje <benjaminlilje@hotmail.com>
2025-05-06 22:24:35 +0200

    fixed local pathing

--- iphone_counter_app/src/main.rs
+++ iphone_counter_app/src/main.rs
@@ -3,17 +3,18 @@ use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
-    // Serve static files from the "docs" directory
-    let static_files = warp::fs::dir("docs");
+    // Serve static files from the "docs" directory under both "/docs" and root paths
+    let static_files_docs = warp::path("docs").and(warp::fs::dir("/running/docs"));
+    let static_files_root = warp::fs::dir("/running/docs");
 
     // Serve the index.html file for the root path
-    let index = warp::path::end().and(warp::fs::file("../index.html"));
+    let index = warp::path::end().and(warp::fs::file("/home/koppen/git/running/index.html"));
 
     // Serve the manifest.json file from the root directory
-    let manifest = warp::path("manifest.json").and(warp::fs::file("../manifest.json"));
+    let manifest = warp::path("manifest.json").and(warp::fs::file("/home/koppen/git/running/manifest.json"));
 
     // Combine the routes
-    let routes = index.or(static_files).or(manifest);
+    let routes = index.or(static_files_docs).or(static_files_root).or(manifest);
 
     // Start the server
     warp::serve(routes)
