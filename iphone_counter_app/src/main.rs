381ca16f6a36766033958e280a638f2f569cd564
Benjamin Lilje <benjaminlilje@hotmail.com>
2025-05-06 20:41:54 +0200

    fixed structure issue and sound

--- iphone_counter_app/src/main.rs
+++ iphone_counter_app/src/main.rs
@@ -1,15 +1,27 @@
 use tokio;
-use warp::Filter;
+use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
     ensure_static_dir();
 
-    // Serve static files (HTML, CSS, JS)
-    let static_files = warp::fs::dir("static");
+    // Serve static files from the "docs" directory
+    let static_files = warp::fs::dir("docs");
+
+    // Serve the index.html file for the root path
+    let index = warp::path::end().map(|| {
+        let index_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("../index.html");
+        let index_content = std::fs::read_to_string(index_path).expect("Failed to read index.html");
+        Response::builder()
+            .header("content-type", "text/html")
+            .body(index_content)
+    });
+
+    // Combine the routes
+    let routes = index.or(static_files);
 
     // Start the server
-    warp::serve(static_files)
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
