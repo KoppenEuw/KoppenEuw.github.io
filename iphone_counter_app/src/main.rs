a1ee71ebedf591724197fe0ae700a961d777cee8
Benjamin Lilje <benjaminlilje@hotmail.com>
2025-05-06 23:04:04 +0200

    works locally, lets go again

--- iphone_counter_app/src/main.rs
+++ iphone_counter_app/src/main.rs
@@ -4,8 +4,8 @@ use warp::{http::Response, Filter};
 #[tokio::main]
 async fn main() {
     // Serve static files from the "docs" directory under both "/docs" and root paths
-    let static_files_docs = warp::path("docs").and(warp::fs::dir("/running/docs"));
-    let static_files_root = warp::fs::dir("/running/docs");
+    let static_files_docs = warp::path("docs").and(warp::fs::dir("/home/koppen/git/running/docs"));
+    let static_files_root = warp::fs::dir("/home/koppen/git/running/docs");
 
     // Serve the index.html file for the root path
     let index = warp::path::end().and(warp::fs::file("/home/koppen/git/running/index.html"));
